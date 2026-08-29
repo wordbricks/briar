57b0074e09604e470f197ff9c67fc07a06b1bfa9
Wordbricks <wordbricks@mango.tail55193.ts.net>
2026-08-29 21:46:43 +0900

    refactor: remove unused component barrels

--- apps/briar/src/components/hunt/editor/index.ts
+++
@@ -1,7 +0,0 @@
-export { CreateIssueDialog } from "./CreateIssueDialog";
-export { DraftIssueDescriptionEditor } from "./DraftIssueDescriptionEditor";
-export { EditIssueDialog } from "./EditIssueDialog";
-export { IssueCheckpointDropdown } from "./IssueCheckpointDropdown";
-export { IssueDescriptionField } from "./IssueDescriptionField";
-export { IssueInlineAttachmentPreview } from "./IssueInlineAttachmentPreview";
-export { SelectedAttachment } from "./SelectedAttachment";
